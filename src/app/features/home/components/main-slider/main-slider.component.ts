62b49d9494105ba5f143ac8018d032e82bfe690a
ahmedalaa404 <az.ahmed.alaa@gmail.com>
2025-03-09 14:19:02 +0200

    Add ngx-owl-carousel-o for main slider and update product card layout

--- src/app/features/home/components/main-slider/main-slider.component.ts
+++ src/app/features/home/components/main-slider/main-slider.component.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+
 
 @Component({
   selector: 'app-main-slider',
-  imports: [],
+  imports: [CarouselModule,CommonModule],
   templateUrl: './main-slider.component.html',
   styleUrl: './main-slider.component.css'
 })
 export class MainSliderComponent {
+
+  slidesStore: string[] = ["images/1680401672268-cover.jpeg","images/1680401672268-cover.jpeg","images/1680401672268-cover.jpeg","images/1680401672268-cover.jpeg"];
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 2
+      }
+    },
+    nav: true
+  }
+
+
 
 }
