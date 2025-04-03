49867cae72d426d1c9430dde82c6969c8e5b5cab
ahmedalaa404 <az.ahmed.alaa@gmail.com>
2025-04-03 02:45:23 +0200

    Implement scroll-to-top button visibility: add logic to show/hide button based on scroll position in main layout component.

--- src/app/core/layouts/main-layout/main-layout.component.ts
+++ src/app/core/layouts/main-layout/main-layout.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
+import { Component, HostListener } from '@angular/core';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [NavbarComponent ,RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.css'
+  styleUrl: './main-layout.component.css',
 })
 export class MainLayoutComponent {
-  GoToUp()
-  {
+  GoToUp() {
     scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }
+
+  showButton:boolean = false; // property to show/hide button
+
+  @HostListener('window:scroll') scrollTotOP(){ // method Decoratore
+    const scrollTop = document.documentElement.scrollTop;
+
+
+    if(scrollTop>500)
+    {
+      this.showButton=true;
     }
+    else
+    {
+      this.showButton=false;
+    }
+  }
 }
